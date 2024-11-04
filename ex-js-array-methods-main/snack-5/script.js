eb21616303f922ba5a6b67ebe177651cfb5c4a19
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-04 17:57:51 +0100

    ADD filter method snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -3,8 +3,6 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
-nums.forEach((num) => {
-    if (num % 2 === 0) {
-        console.log(num)
-    }
-})
+const evenNums = nums.filter((num) => num % 2 == 0)
+
+console.log(evenNums)
