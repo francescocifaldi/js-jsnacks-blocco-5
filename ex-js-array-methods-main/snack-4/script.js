47c62ca001bfab7b7ded1d91e2c45f4925d03b15
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-04 18:02:47 +0100

    ADD map method snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -8,4 +8,6 @@ const posts = [
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
-posts.forEach((post) => console.log(post.author))
+const authors = posts.map((post) => post.author)
+
+console.log(authors)
