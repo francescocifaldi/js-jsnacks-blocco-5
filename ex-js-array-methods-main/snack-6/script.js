5bfaca044bc4ce0ae5610d56b231376172ffcecf
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-04 16:27:26 +0100

    ADD snack 6

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,19 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const shortZucchine = []
+const longZucchine = []
+
+zucchine.forEach((zucchina) => {
+  if (zucchina.length > 15) {
+    longZucchine.push(zucchina)
+  }
+  else {
+    shortZucchine.push(zucchina)
+  }
+})
+
+console.log(zucchine)
+console.log(shortZucchine)
+console.log(longZucchine)
