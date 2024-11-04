9cb9e9e9d3646798bc6d10e4edb5340791bb73c3
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-04 16:31:18 +0100

    ADD snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+students.forEach((student) => {
+  if (student.name === 'Marco Lanci') {
+    console.log(student.class)
+  }
+})
